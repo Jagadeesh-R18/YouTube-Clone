80859ab010e8ce93f6b7b49f146dd74754dd8807
“Jagadeesh <jagadeeshrajaput18@gmail.com>
2024-03-26 14:34:02 +0530

    youtube clone project

---
+++ src/data.js
@@ -0,0 +1,13 @@
+export const API_KEY = 'AIzaSyA5QuZdKTcFsVaJN20A7dohQ6wevbGwhrE';
+
+export const value_converter = (value) =>{
+
+    if(value >=100000){
+        return Math.floor(value/100000)+"M"
+    }else if(value >= 1000){
+        return Math.floor(value/1000)+"K"
+    }else{
+        return value;
+    }
+
+ } 
